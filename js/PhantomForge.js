8c94f12938059cadaade6ad331da85deef8a4f3a
Orim12 <miro.vaassen@gmail.com>
2024-01-11 11:58:39 +0100

    update

--- js/PhantomForge.js
+++ js/PhantomForge.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
         // Navigeer naar de gewenste website
         window.location.href = "https://discord.gg/4fTFXz5W";
     });
-
-
     // Zoek de knop op basis van het id-attribuut
     var discord_Voodoo = document.getElementById("discord_Voodoo");
 
@@ -19,5 +17,13 @@ document.addEventListener("DOMContentLoaded", function() {
     discord_Voodoo.addEventListener("click", function() {
         // Navigeer naar de gewenste website
         window.location.href = "https://discord.gg/3RNDpNGb8r";
+    });
+    // Zoek de knop op basis van het id-attribuut
+    var more_info_voodoo = document.getElementById("more_info_voodoo");
+
+    // Voeg een click-eventlistener toe aan de knop
+    more_info_voodoo.addEventListener("click", function() {
+        // Navigeer naar de gewenste website
+        window.location.href = "../Voodoo-Fortress-Website-main/extra-html/more-information.html";
     });
 });
