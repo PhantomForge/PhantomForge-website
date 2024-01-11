ea0dab02553dfa12319533b161ed98f60966926c
Orim12 <miro.vaassen@gmail.com>
2024-01-11 10:08:01 +0100

    Update PhantomForge.js

--- js/PhantomForge.js
+++ js/PhantomForge.js
@@ -10,11 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // Navigeer naar de gewenste website
         window.location.href = "https://discord.gg/4fTFXz5W";
     });
-});
-// script.js
 
-// Wacht tot het DOM (Document Object Model) is geladen
-document.addEventListener("DOMContentLoaded", function() {
+
     // Zoek de knop op basis van het id-attribuut
     var discord_Voodoo = document.getElementById("discord_Voodoo");
 
