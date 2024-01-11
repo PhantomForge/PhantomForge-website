9e934c49e0f2369548dedf248d9c8e4e5c0ae5fe
Orim12 <miro.vaassen@gmail.com>
2024-01-11 10:04:13 +0100

    update

--- js/PhantomForge.js
+++ js/PhantomForge.js
@@ -1,4 +1,4 @@
-// script.js
+// script.jsVoodoo
 
 // Wacht tot het DOM (Document Object Model) is geladen
 document.addEventListener("DOMContentLoaded", function() {
@@ -9,5 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
     discord.addEventListener("click", function() {
         // Navigeer naar de gewenste website
         window.location.href = "https://discord.gg/4fTFXz5W";
+    });
+});
+// script.js
+
+// Wacht tot het DOM (Document Object Model) is geladen
+document.addEventListener("DOMContentLoaded", function() {
+    // Zoek de knop op basis van het id-attribuut
+    var discord_Voodoo = document.getElementById("discord_Voodoo");
+
+    // Voeg een click-eventlistener toe aan de knop
+    discord_Voodoo.addEventListener("click", function() {
+        // Navigeer naar de gewenste website
+        window.location.href = "https://discord.gg/3RNDpNGb8r";
     });
 });
